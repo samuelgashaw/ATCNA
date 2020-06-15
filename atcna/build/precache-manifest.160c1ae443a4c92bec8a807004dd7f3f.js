93d00a2e9555345875d0128d22d5050bdc37c4ff
Samuel Gashaw <samuelmgashaw@gmail.com>
2020-06-14 23:10:16 -0400

    build folder

---
+++ atcna/build/precache-manifest.160c1ae443a4c92bec8a807004dd7f3f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424027bf80a6f3786983a5a68aa18db6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e22d0e56bf569ffe380f",
+    "url": "/static/css/2.5c0038b0.chunk.css"
+  },
+  {
+    "revision": "6f5f436193a884ddf884",
+    "url": "/static/css/main.635b4721.chunk.css"
+  },
+  {
+    "revision": "e22d0e56bf569ffe380f",
+    "url": "/static/js/2.beb9b203.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.beb9b203.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f5f436193a884ddf884",
+    "url": "/static/js/main.53d0ee9a.chunk.js"
+  },
+  {
+    "revision": "184d28001446663963a6",
+    "url": "/static/js/runtime-main.3cf5c4eb.js"
+  },
+  {
+    "revision": "96a574ddedb820439e6122310cdde149",
+    "url": "/static/media/logo.96a574dd.png"
+  }
+]);
