a0c44e175151fb5bdfc6e4fb31aa32b06b79e660
Samuel Gashaw <samuelmgashaw@gmail.com>
2020-08-24 15:55:55 -0400

    update on about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -19,8 +19,8 @@ class About extends Component {
                 our hometown Tebassie, Debre Birhan, Ethiopia. We are a North
                 America based Non-Governmental Organization (NGO) that offers
                 support to most vulnerable groups in our hometown to gain access
-                to health services, clean water, and education.Founded by the
-                Tebassie Hometown Association (HTA) group, we understand the
+                to health services, clean water, and education. Founded by the
+                Tebassie Hometown Association (THA) group, we understand the
                 need of our hometown and the work ahead of us to provide aid in
                 communities where the capacity of the local government has
                 fallen short. We are confident that with our unwavering support,
